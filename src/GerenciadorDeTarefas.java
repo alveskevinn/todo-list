279888f3ec29c27b8051462d0cb40c100312cf9b
Alveskevinn <kevinpedrosodev@gmail.com>
2025-02-03 00:01:53 -0300

    feat: getDescrição

---
+++ src/GerenciadorDeTarefas.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+class GerenciadorDeTarefas {
+    private ArrayList<Tarefa> tarefas;
+
+    public GerenciadorDeTarefas() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefas.add(new Tarefa(descricao));
+        System.out.println("Tarefa adicionada com sucesso!");
+    }
+
+    public void listarTarefas() {
+        if (tarefas.isEmpty()) {
+            System.out.println("Nenhuma tarefa cadastrada.");
+        } else {
+            for (int i = 0; i < tarefas.size(); i++) {
+                System.out.println((i + 1) + ". " + tarefas.get(i));
+            }
+        }
+    }
+
+    public void marcarTarefaComoConcluida(int indice) {
+        if (indice >= 0 && indice < tarefas.size()) {
+            tarefas.get(indice).marcarComoConcluida();
+            System.out.println("Tarefa marcada como concluída!");
+        } else {
+            System.out.println("Índice inválido.");
+        }
+    }
+
+    public void removerTarefa(int indice) {
+        if (indice >= 0 && indice < tarefas.size()) {
+            tarefas.remove(indice);
+            System.out.println("Tarefa removida com sucesso!");
+        } else {
+            System.out.println("Índice inválido.");
+        }
+    }
+}
